139b382e1cfc9a5d1b1ed87181f347f8e4b46b0c
akin <akinkayrau@proton.me>
2026-04-29 11:45:08 +0300

    base ui scheme

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
 import { Entity, Player } from "./entity.js";
 import { Coordinate } from "./utils.js";
 import { Viewport } from "./viewport.js";
-import { MainMenu, SpaceScene } from "./ui.js"
-
+import { MainMenu, SpaceScene, StatusPane, TerminalPane } from "./ui.js"
 
 const entity = new Entity(new Coordinate(0, 0), "Player");
 const entityOther = new Entity(new Coordinate(100, 200), "Other");
-
 
 class App {
     start() {
@@ -17,20 +15,18 @@ class App {
         this.player = new Player(canvas, entity);
         this.mainMenu = new MainMenu(canvas);
         this.spaceScene = new SpaceScene(canvas);
+        this.statusPane = new StatusPane(canvas);
+        this.statusPane = new StatusPane(canvas);
+        this.TerminalPane = new TerminalPane(canvas);
 
         this.lastTime = 0;
 
-        canvas.addEventListener("click", (e) => {
-        });
+        this.mainMenu.playBtn.addEventListener("click", () => this.mainMenuHide());
 
         window.addEventListener('keydown', (e) => {
             if (e.key.toLowerCase() === 'j') {
                 this.player.entity = (this.player.entity === entity) ? entityOther : entity;
                 this.viewport.coordinate = this.player.entity.center;
-            }
-            if (e.key.toLowerCase() === 'm') {
-                this.mainMenu.visible = !this.mainMenu.visible;
-                this.spaceScene.visible = !this.spaceScene.visible;
             }
             if (e.key.toLowerCase() === '=') {
                 this.viewport.zoom += 0.1;
@@ -41,6 +37,8 @@ class App {
                 this.viewport.zoom = (this.viewport.zoom < 0.5) ? 0.5 : this.viewport.zoom;
             }
         });
+        // ENABLE main menu from here.
+        // this.mainMenuShow();
     }
 
     update(currentTime) {
@@ -49,16 +47,18 @@ class App {
         this.lastTime = currentTime;
 
         // Make logic updates here
-        if (this.mainMenu.visible) this.mainMenu.update(deltaTime);
         if (this.spaceScene.visible) this.spaceScene.update(deltaTime);
 
         entity.update(deltaTime);
         this.player.update(deltaTime);
+        this.TerminalPane.update(deltaTime);
 
         // Make drawings here
         this.viewport.reset();
         this.spaceScene.draw();
         this.mainMenu.draw();
+        this.statusPane.draw();
+        this.TerminalPane.draw();
 
         if (!(this.mainMenu.fullscreen && this.mainMenu.visible)) {
             this.viewport.transform();
@@ -69,9 +69,21 @@ class App {
         // keep the animation going by requesting another animation frame
         requestAnimationFrame((time) => this.update(time));
     }
-
     clearCanvas() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    mainMenuShow() {
+        this.mainMenu.on();
+        this.spaceScene.visible = true;
+    }
+
+    mainMenuHide() {
+        this.mainMenu.off();
+        this.spaceScene.visible = false;
+
+        this.statusPane.visible = true;
+        this.TerminalPane.visible = true;
     }
 }
 
@@ -79,4 +91,4 @@ class App {
 const app = new App();
 app.start();
 app.ctx.save();
-requestAnimationFrame((time) => app.update(time))
+requestAnimationFrame((time) => app.update(time));
--- js/ui.js
+++ js/ui.js
@@ -29,34 +29,33 @@ class Window {
 export class MainMenu extends Window {
     constructor(canvas) {
         super(canvas, canvas.width, canvas.height, 0, 0);
+        this.container = document.getElementById("mainMenu");
+        this.playBtn = document.getElementById("playButton");
+    }
+
+    on() {
+        this.container.style.display = "flex";
+        this.visible = true;
+    }
+
+    off() {
+        this.container.style.display = "none";
+        this.visible = false;
     }
 
     update() { }
 
-    show() {
-        this.ctx.fillStyle = "white";
-        this.ctx.textAlign = "center";
-        this.ctx.font = "50px Monoton";
-        this.ctx.fillText("HIDE  US  G8000", this.width / 2, this.height / 8, this.width);
-    }
-
+    show() { }
 }
 
 export class SpaceScene extends Window {
     constructor(canvas) {
         super(canvas, canvas.width, canvas.height, 0, 0);
-        this.backcanvas = document.createElement("canvas");
-        this.backctx = this.backcanvas.getContext("2d");
-
-        const dimension = Math.ceil(Math.sqrt(this.canvas.width ** 2 + this.canvas.height ** 2));
-
-        this.backcanvas.width = dimension;
-        this.backcanvas.height = dimension;
 
         this.centerX = this.canvas.width / 2;
         this.centerY = this.canvas.height / 2;
 
-        this.starDensity = 0.0005;
+        this.starDensity = 0.001;
         this.starCount = 0;
         this.calculateStarCount();
 
@@ -64,6 +63,8 @@ export class SpaceScene extends Window {
 
         this.stars = [];
 
+        this.smallMode = true;
+        this.visible = true;
         this.generateSpace();
     }
 
@@ -72,23 +73,27 @@ export class SpaceScene extends Window {
             const star = { x: (Math.random() - 0.5) * this.canvas.width, y: (Math.random() - 0.5) * this.canvas.height, z: Math.random() };
             this.stars.push(star);
         }
-
-        this.backctx.translate(this.centerX / 2 + this.canvas.width / 2, this.centerY + this.canvas.height / 2);
     }
 
     show() {
+        let width = this.canvas.width;
+        let x = 0;
+        if (this.smallMode) {
+            x = this.canvas.width / 4;
+            width = this.canvas.width * 5 / 12;
+        }
         this.ctx.fillStyle = "black";
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.fillRect(x, 0, width, this.canvas.height);
 
         this.ctx.fillStyle = "white";
         this.stars.forEach(star => {
             const zdis = star.z - this.bgZ;
             if (zdis < 0) {
-                star.z = this.bgZ + 1.5;
+                star.z = this.bgZ + 1 + Math.random();
                 star.x = (Math.random() - 0.5) * this.canvas.width * 2;
                 star.y = (Math.random() - 0.5) * this.canvas.height * 2;
             }
-            const size = Math.min(20, 1 / zdis);
+            const size = Math.min(20, 1.25 / zdis);
 
             this.ctx.fillRect(this.centerX + star.x / zdis, this.centerY + star.y / zdis, size, size);
         });
@@ -98,14 +103,66 @@ export class SpaceScene extends Window {
         this.bgZ += 0.1 * dt;
         //this.bgX -= 10 * dt;
         //this.bgY -= 10 * dt;
-
-        if (this.bgX > this.backcanvas.width / 2 || this.bgX < 0)
-            this.bgX = (this.backcanvas.width - this.canvas.width) / 2;
-        if (this.bgY > this.backcanvas.height / 2 || this.bgY < 0)
-            this.bgY = (this.backcanvas.height - this.canvas.height) / 2;
     }
 
     calculateStarCount() {
-        this.starCount = Math.round(this.backcanvas.width * this.backcanvas.height * this.starDensity);
+        this.starCount = Math.round(this.canvas.width * this.canvas.height * this.starDensity);
+    }
+}
+
+export class StatusPane extends Window {
+    constructor(canvas) {
+        super(canvas, canvas.width / 4, canvas.height, 0, 0);
+        this.visible = true;
+    }
+
+    show() {
+    }
+
+    update() { }
+}
+
+export class TerminalPane extends Window {
+    constructor(canvas) {
+        super(canvas, canvas.width / 3, canvas.height, canvas.width * 2 / 3, 0);
+        this.visible = true;
+        this.container = document.getElementById("terminalPane");
+        this.pendingText = [];
+        this.addText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 10);
+        this.addText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 20);
+        this.addText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 30);
+        console.log(this.pendingText);
+    }
+
+    addText(string, letterPerSec) {
+        const textElement = document.createElement("div");
+        this.container.appendChild(textElement);
+        if (letterPerSec <= 0) {
+            textElement.textContent = string;
+        } else {
+            this.pendingText.push({ str: string, letterPerSec: letterPerSec, current: 0, progress: 0, element: textElement });
+        }
+    }
+
+    show() {
+    }
+
+    update(dt) {
+        for (let i = 0; i < this.pendingText.length; i++) {
+            const text = this.pendingText[i];
+
+            text.progress += dt;
+
+            while (text.progress > 1 / text.letterPerSec) {
+                text.element.textContent += text.str.at(text.current);
+                text.current++;
+                if (text.current >= text.str.length) {
+                    this.pendingText.splice(i, 1);
+                    --i;
+                    break;
+                }
+                text.progress -= 1 / text.letterPerSec;
+            }
+        }
     }
 }
